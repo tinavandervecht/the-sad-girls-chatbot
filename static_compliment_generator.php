ffe58ae0958af87402dc9cdaf02b145e9350b030
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-02-11 20:43:17 -0500

    Building out logic around when to send compliment

--- static_compliment_generator.php
+++ static_compliment_generator.php
@@ -1,34 +1,39 @@
 <?php
-    $credentials = json_decode(file_get_contents('./credentials.json'));
-    $compliments = array();
-    // Compliments
-    array_push($compliments, 'You are so skilled at your job! I\'m so jealous of your work ethic.');
+    date_default_timezone_set('est');
+    $hour = date('G');
 
-    $data = array(
-        'channel'     => '#general',
-        'username'    => 'The Saddest Girl',
-        'icon_emoji'  => ':woman-pouting::skin-tone-2:',
-        'attachments' => array([
-            'color'    => '#ffb0b0',
-            'fields'   => array(
-                [
-                    'title' => 'It\'s been too long since anyone complimented you!',
-                    'value' => $compliments[rand(1,sizeof($compliments)) - 1],
-                    'short' => false
-                ]
-            )
-        ])
-    );
+    if (rand(0,1) == 1 && ($hour > '09' && $hour < '17')) {
+        $credentials = json_decode(file_get_contents('./credentials.json'));
+        $compliments = array();
+        // Compliments
+        array_push($compliments, 'You are so skilled at your job! I\'m so jealous of your work ethic.');
 
-    $data_string = json_encode($data);
-    $ch = curl_init($credentials[0]->slackHook);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($data_string))
+        $data = array(
+            'channel'     => '#general',
+            'username'    => 'The Saddest Girl',
+            'icon_emoji'  => ':woman-pouting::skin-tone-2:',
+            'attachments' => array([
+                'color'    => '#ffb0b0',
+                'fields'   => array(
+                    [
+                        'title' => 'It\'s been too long since anyone complimented you!',
+                        'value' => $compliments[rand(1,sizeof($compliments)) - 1],
+                        'short' => false
+                    ]
+                )
+            ])
         );
 
-    curl_exec($ch);
+        $data_string = json_encode($data);
+        $ch = curl_init($credentials[0]->slackHook);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string))
+            );
+
+        curl_exec($ch);
+    }
  ?>
